d45bc8a91ab3088d6e330faa28511896a0884f82
Aram Simonian <aram.contacts@gmail.com>
2017-09-12 20:04:32 +0100

    Challenges 5 & 6

--- session3/challenge/6_arguments.rb
+++ session3/challenge/6_arguments.rb
@@ -17,3 +17,13 @@
 # match_maker true,  true,  true, true, nil     # => [false, true]
 # match_maker true,  true,  true, 0, nil        # => [false, true]
 
+def match_maker(oppositey,*truthy_pairs)
+  output = []
+  
+  if oppositey
+    truthy_pairs.each_slice(2) { |a, b| output << (!!a != !!b) }
+  else
+    truthy_pairs.each_slice(2) { |a, b| output << (!!a == !!b) }
+  end
+  output
+end
